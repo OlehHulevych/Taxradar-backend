835e1ec78c810a730a12f06b1a7bc9e07c76c954
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-08-18 09:53:55 +0200

    added taxThreshold service

---
+++ src/main/java/com/taxradar/backend/api/controllers/TaxThresholdController.java
@@ -0,0 +1,11 @@
+package com.taxradar.backend.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/taxthreshold")
+public class TaxThresholdController {
+
+    private
+}
